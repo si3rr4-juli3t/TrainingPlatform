f6a5d04a2fb8d406e24f482c31f8b12354e8ddee
sjones <sjones@ANSGRP-002753>
2019-11-04 15:55:53 +0000

    Initial Commit

---
+++ TrainingPlatform.API/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TrainingPlatform.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrainingPlatform.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Value> Values { get; set; }
+ 
+    }
+}
